c09cf8190bbd6df730df1cb820796a9e3fb78640
SebastianPegoraro <30633725+SebastianPegoraro@users.noreply.github.com>
2017-11-10 09:19:58 -0300

    Add files via upload

---
+++ Smooth Auto-Scroll/smoothAuto-Scroll.js
@@ -0,0 +1,17 @@
+$("#consulta").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".consul").offset().top},
+        'slow');
+});
+
+$("#normativa").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".norm").offset().top},
+        'slow');
+});
+
+$("#formulario").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".formu").offset().top},
+        'slow');
+});
